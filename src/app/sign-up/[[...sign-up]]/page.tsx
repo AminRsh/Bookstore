933be5b36d60786efa5bdde533d9039259c41732
AminRsh <amin.sharifi45@gmail.com>
2024-04-15 21:38:20 -0400

    changing_route

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,5 +2,5 @@ import { SignUp } from "@clerk/nextjs";
 
 
 export default function Page() {
-    return <SignUp />;
+    return <div className="flex flex-col justify-center items-center my-16"><SignUp /></div> ;
 }
