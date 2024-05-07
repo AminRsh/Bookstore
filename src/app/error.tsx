2843155d9ec616088fcc05a63bab147c26e8b9f9
AminRsh <amin.sharifi45@gmail.com>
2024-05-07 12:18:16 -0400

    Error_Page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,19 @@
 "use client"
 import H1 from '@/components/ui/h1';
 
+interface ErrorProps {
+    statusCode: number;
+    error: Error; 
+}
 
-const Error = () => {
+
+const Error = ({ statusCode, error }: ErrorProps) => {
+    console.log(error);
+    
     return (
         <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
             <H1>Error</H1>
-            <p className="">An unexpected error occured</p>
+            <p className="">An {statusCode} error occured</p>
         </main>
     )
 }
